2fa6816d22d920ac67f7cd4ed6cbc673250904d7
Jonas Laursen <jona8690@edu.eal.dk>
2019-02-04 16:37:18 +0100

    Started on models

---
+++ DBD/Assignments/A2 E3 - ORM CF/A2 E3 - ORM CF/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A2_E3___ORM_CF.Models
+{
+	public class Employee
+	{
+		public int CPR_No { get; set; }
+		public string Fname { get; set; }
+		public string Lname { get; set; }
+		public string Name { get { return this.Fname + " " + this.Lname; } }
+
+		public string Adr_Street { get; set; }
+		public int Adr_Zip { get; set; }
+		public string Adr_City { get; set; }
+		public int Phone { get; set; }
+		public string Email { get; set; }
+
+		public Department WorksFor { get; set; }
+	}
+}
