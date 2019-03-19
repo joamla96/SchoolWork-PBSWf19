1b53487e7966cc0fd5942aa392d17867dad6b028
Jonas Laursen <jona8690@edu.eal.dk>
2019-03-19 20:27:03 +0100

    Ex 2 and 3

---
+++ DLS/Week11/Week11/DidYouMean.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Week11 {
+	public class DidYouMean {
+		/// Did You Mean should contain multiple functions
+		/// 1. Spell check
+		///		The idea of this method is that a string is inputtet and a string array returned.
+		///		The inputted string should be checked for generic typing mistakes.
+		///		Example, could be using levenshtein to check the input against common words
+		///		and return the array orderd with closer words first, or above a certain threashold.
+		///	2. Alternative Meanings
+		///		Words can have multiple meanings, this method takes a string input, a single word, and returns an array of synonyms
+		///		For example, bold => brave, strong, daring
+		///		Other types of alternatives, e.g. the user typing in Bill Gates
+		///		could also want references to Melinda Gates, or the Bill & Melinda Gates Foundation, climate change, ect.
+		///		Corrolation between words.
+	}
+}
---
+++ DLS/Week11/Week11/StorageSystem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Week11 {
+	public class StorageSystem {
+		///	3. Supposed you are in team that should develop a storage system.
+		///	The storage contains pallets with bottles of - lets say wine.
+		///	A function in the system is about reservation of pallets to a given order. 
+		///	These pallets do not contain the same amount of bottles.
+		///	One way to reserve pallets to an order is to reserve a set of pallets, 
+		///	such that the total amount of bottles is minimal but greater than or equal to the amount of bottles ordered.
+		///		a. Give a precise description of this requirement.
+		///		b. Make a specification of a component that can provide functionality for selecting the set of pallets to a given order.
+		///		
+
+		/// The system should take an input of an integer.
+		/// It should then look up a list of pallets, 
+		/// and pick them out in a way which returns the least amount of bottles
+		/// that is euqal to or above the input.
+		/// E.g. order list of pallets by number of bottles, least first
+		/// take from the top, untill total count equals or surpasses the input.
+		/// 
+		/// Alternatively, in order to keep more pallets of smaller sizes for smaller orders
+		/// Take the largest pallet, that does not exceed the input.
+		/// Subtract amount of bottles from the input.
+		/// Find the larget pallet, that does not exceed the remainder
+		/// Repeat untill out of pallets, then take the smallest available pallet.
+	}
+}
